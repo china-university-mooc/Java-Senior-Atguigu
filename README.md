b11104edd3a57c02a384a92a1e5ed99c84fb63aa
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-04-13 14:30:12 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java Senior Atguigu
---
+++ src/com/itutry/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.itutry;
+
+/**
+ * @author itutry
+ * @create 2020-04-13_12:23
+ */
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    System.out.println("HelloWorld");
+  }
+}
